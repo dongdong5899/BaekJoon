21372a1a6302c32a25146d2b19e676080ac58068
dongdong5899 <123608053+dongdong5899@users.noreply.github.com>
2024-08-06 09:02:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 376 ms, Memory: 42100 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,30 +1,17 @@
 #include <iostream>
 #include <unordered_set>
-#include <unordered_map>
 
 using namespace std;
 
-class pos 
-{
-public :
-	int x;
-	int y;
-
-	bool operator==(const pos& p)
-	{
-		return x == p.x || x == p.y || y == p.x || y == p.y;
-	}
-};
-
 unordered_set<int> numSet;
-unordered_map<int, unordered_set<int>> linkMap;
+unordered_set<int>* linkMap;
 
 void addSet(int n)
 {
-	if (numSet.find(n) != numSet.end()) return;
 	numSet.insert(n);
 	for (int num : linkMap[n])
 	{
+		if (numSet.find(num) != numSet.end()) continue;
 		addSet(num);
 	}
 }
@@ -36,23 +23,19 @@ int main()
 
 	int N, M, count = 0;
 	cin >> N >> M;
-
-	pos arr[1000];
+	linkMap = new unordered_set<int>[N + 1];
 
 	for (int i = 0; i < M; ++i)
 	{
 		int x, y; 
 		cin >> x >> y;
-		if (linkMap.find(x) == linkMap.end())
-			linkMap.insert({ x, unordered_set<int>() });
-		if (linkMap.find(y) == linkMap.end())
-			linkMap.insert({ y, unordered_set<int>() });
 		linkMap[x].insert(y);
 		linkMap[y].insert(x);
 	}
 	for (int i = 1; i <= N; ++i)
 	{
-		if (numSet.find(i) == numSet.end()) ++count;
+		if (numSet.find(i) != numSet.end()) continue;
+		++count;
 		addSet(i);
 	}
 	cout << count;
